a713ce3849a9e2f9c5e4bc61a23a647b19a79ecd
bishesh63@gmail.com <bishesh63@gmail.com>
2019-12-26 23:43:14 +0545

    complete

---
+++ app/src/main/java/com/example/bishesh_button_nav/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,30 @@
+package com.example.bishesh_button_nav.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.bishesh_button_nav.R;
+
+public class NotificationsFragment extends Fragment {
+
+
+    private WebView webView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        webView = (WebView) root.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://softwarica.edu.np/");
+        return root;
+    }
+}
